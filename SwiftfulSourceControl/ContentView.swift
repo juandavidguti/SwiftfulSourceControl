d2adfbe69ee7fcab783a37f557f494bb3c232caf
Juan David <juandaguti@icloud.com>
2025-06-18 14:39:59 +0100

    [Feature] Remove click me button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -38,10 +38,6 @@ struct ContentView: View {
             Button("Subscribe") {
                 
             }
-            Button("Click me") {
-                
-            }
-            .background(Color.red)
         }
         .padding()
     }
