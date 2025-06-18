221d23817e3f19c1c48d840fd8e5eb942c22f56a
Juan David <juandaguti@icloud.com>
2025-06-18 14:24:40 +0100

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Juan David Gutierrez Olarte on 18/06/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+        
+    }
+}
+
+#Preview {
+    HomeView()
+}
