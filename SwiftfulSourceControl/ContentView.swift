1c3ca3ad435710a1b2956181e7c7f5abed81b7dd
Juan David <juandaguti@icloud.com>
2025-06-18 14:32:17 +0100

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Juan David Gutierrez Olarte on 16/06/25.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
