7cda76b66d2d47efe90da64b57820f4a63af5706
Juan David <juandaguti@icloud.com>
2025-06-22 16:48:11 +0100

    [Clean] Doc info updated

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -17,6 +17,8 @@
  Rebase = Moving one branch on top of another branch
  Cherry picking = Duplicating (copying) one commit from one branch to another
  Pull Request (PR) = Request to merge branch
+ PR Merge = Merge all commits
+ PR Squash and Merge = Squash all commits into one and then merge ONE commit
  
  
  COMMIT MESSAGES
