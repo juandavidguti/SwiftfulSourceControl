cd91e6e9041f02dba3071721f535cdd04f94e219
Juan David <juandaguti@icloud.com>
2025-06-22 23:46:33 +0100

    [Feature] Add profile View

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Juan David Gutierrez Olarte on 22/06/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("JuanDavid")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
